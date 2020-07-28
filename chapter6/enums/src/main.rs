11fcf428c11f829f0b3096dd0f447a0ec4503680
mikhailyuki <mikhailyuki>
2020-07-28 11:15:37 +0900

    6.1.Enumを定義する

---
+++ chapter6/enums/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // let sum = x + y;
+ }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) { }
+}
+
+// fn route(ip_type: IpAddrKind) {
+//     println!("Function is called.")
+// }
